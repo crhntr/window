785703ffcb1c5af15a5c3f516abf697b2c0c9fbc
Christopher Hunter <hunter.christopher@icloud.com>
2020-05-04 13:03:58 -0700

    remove commented stubs

--- document.go
+++ document.go
@@ -22,11 +22,6 @@ func (_ document) Set(key string, value interface{})           { doc.Set(key, va
 func (_ document) Call(m string, args ...interface{}) js.Value { return doc.Call(m, args...) }
 func (_ document) Type() js.Type                               { return doc.Type() }
 
-// func (_ document) Truthy() bool { return doc.Truthy() }
-// func (_ document) IsNull() bool { return doc.IsNull() }
-// func (_ document) IsUndefined() bool { return doc.IsUndefined() }
-// func (_ document) InstanceOf(t js.Value) bool { return doc.InstanceOf(t) }
-
 func (el document) AddEventListener(eventName string, listener EventListener) {
 	el.Call("addEventListener", eventName, js.FuncOf(func(_ js.Value, args []js.Value) interface{} {
 		listener.HandleEvent(Event(args[0]))
